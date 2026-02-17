5f4abfed3d00feb184866be25e5bfdd1668d2bbd
Vedika Toke <vedikatoke1211@gmail.com>
2026-02-17 14:36:12 +0530

    Refactor todo functions for clarity and conciseness

--- src/App.js
+++ src/App.js
@@ -7,57 +7,52 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 
-    // Load todos from localStorage when app starts
-    const [todos, setTodos] = useState(() => {
-        let savedTodos = localStorage.getItem("todos");
-        if (savedTodos) {
-            return JSON.parse(savedTodos);
-        } else {
-            return [];
-        }
-    });
+  // Load todos from localStorage on first load
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
 
-    // Save todos to localStorage whenever todos change
-    useEffect(() => {
-        localStorage.setItem("todos", JSON.stringify(todos));
-    }, [todos]);
+  // Save todos whenever they change
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-    // Delete function
-    const onDelete = (todo) => {
-        console.log("Deleting todo:", todo);
-        setTodos(todos.filter((e) => {
-            return e !== todo;
-        }));
+  // Delete Todo
+  const onDelete = (todo) => {
+    setTodos(todos.filter((e) => e !== todo));
+  };
+
+  // Add Todo
+  const addTodo = (title, desc) => {
+    let sno;
+    if (todos.length === 0) {
+      sno = 1;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc
     };
 
-    // Add function
-    const addTodo = (title, desc) => {
-        let sno;
-        if (todos.length === 0) {
-            sno = 1;
-        } else {
-            sno = todos[todos.length - 1].sno + 1;
-        }
+    setTodos([...todos, myTodo]);
+  };
 
-        const myTodo = {
-            sno: sno,
-            title: title,
-            desc: desc
-        };
-
-        setTodos([...todos, myTodo]);
-    };
-
-    return (
-        <>
-            <Header title="My Todos List" searchBar={true} />
-            <Addtodo addTodo={addTodo} />
-            <Todos todos={todos} onDelete={onDelete} />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header title="My Todos List" searchBar={true} />
+      <Addtodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
-
-
