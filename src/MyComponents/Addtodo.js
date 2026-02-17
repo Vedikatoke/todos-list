ff526ea94a70b944f07ee79278a744869cd2bb98
Vedika Toke <vedikatoke1211@gmail.com>
2026-02-17 14:34:38 +0530

    Update Addtodo.js

--- src/MyComponents/Addtodo.js
+++ src/MyComponents/Addtodo.js
@@ -1,22 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export const Addtodo = () => {
+export const Addtodo = (props) => {
+
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault(); // prevents page reload
+
+    if (!title || !desc) {
+      alert("Title or Description cannot be empty");
+      return;
+    }
+
+    props.addTodo(title, desc);
+    setTitle("");
+    setDesc("");
+  };
+
   return (
     <div className="container my-3">
-        <h3>Add a Todo</h3>
-      <form>
-  <div class="mb-3">
-    <label for="title" class="form-label">todo title</label>
-    <input type="text" class="form-control" id="title" aria-describedby="emailHelp"/>
-   </div>
-  <div class="mb-3">
-    <label for="desc" class="form-label">todo description</label>
-    <input type="text" class="form-control" id="desc"/>
-  </div>
-  
-  <button type="submit" class="btn btn-success">Add Todo</button>
-</form>
-</div>
-  )
-}
-export default Addtodo
+      <h3>Add a Todo</h3>
+      <form onSubmit={submit}>
+        
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">Todo Title</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="desc" className="form-label">Todo Description</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            id="desc"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        </div>
+        
+        <button type="submit" className="btn btn-success">
+          Add Todo
+        </button>
+
+      </form>
+    </div>
+  );
+};
+
+export default Addtodo;
